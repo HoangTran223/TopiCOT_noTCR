73ed23e3a4024843ddd0f9d80a96cfa340bd9004
HoangTran223 <tranvuonghoang204@gmail.com>
2024-09-18 18:28:17 +0700

    09/18/2024: Friendly SAM

---
+++ topmost/trainers/SAM/FSAM.py
@@ -0,0 +1,79 @@
+﻿import torch 
+
+class FriendlySAM(torch.optim.Optimizer):
+    def __init__(self, params, base_optimizer, rho=0.05, sigma=1, lmbda=0.9, adaptive=False, **kwargs):
+        defaults = dict(rho=rho, adaptive=adaptive, **kwargs)
+        super(FriendlySAM, self).__init__(params, defaults)
+        self.sigma = sigma
+        self.lmbda = lmbda
+
+        self.base_optimizer = base_optimizer(self.param_groups, **kwargs)
+        self.param_groups = self.base_optimizer.param_groups
+        self.defaults.update(self.base_optimizer.defaults)
+    
+
+    def _grad_norm(self):
+        norm = torch.norm(
+                    torch.stack([
+                        ((torch.abs(p) if group["adaptive"] else 1.0) * p.grad).norm(p=2)
+                        for group in self.param_groups for p in group["params"] if p.grad is not None ]),
+                    p=2)
+        return norm
+
+
+    @torch.no_grad()
+    def first_step(self, zero_grad=False):
+
+        for group in self.param_groups:
+            for p in group["params"]:      
+                if p.grad is None: continue       
+                grad = p.grad.clone()
+                if not "momentum" in self.state[p]:
+                    self.state[p]["momentum"] = grad
+                else:
+                    # Compute d_t
+                    p.grad -= self.state[p]["momentum"] * self.sigma            
+
+                    # Compute m_t
+                    self.state[p]["momentum"] = self.state[p]["momentum"] * self.lmbda + grad * (1 - self.lmbda)
+            
+        grad_norm = self._grad_norm()
+        for group in self.param_groups:
+            scale = group["rho"] / (grad_norm + 1e-12)
+
+            for p in group["params"]:
+                if p.grad is None: continue
+                self.state[p]["old_p"] = p.data.clone()
+                e_w = (torch.pow(p, 2) if group["adaptive"] else 1.0) * p.grad * scale.to(p)
+                
+                # Compute: w + e(w)
+                p.add_(e_w)                             
+
+        if zero_grad: self.zero_grad()
+
+
+    @torch.no_grad()
+    def second_step(self, zero_grad=False):
+        for group in self.param_groups:
+            for p in group["params"]:
+                if p.grad is None: continue
+
+                # Get back to w from w + e(w)
+                p.data = self.state[p]["old_p"]        
+        
+        # Update
+        self.base_optimizer.step()                      
+
+        if zero_grad: self.zero_grad()
+
+    @torch.no_grad()
+    def step(self, closure=None):
+        # Closure do a full forward-backward pass
+        closure = torch.enable_grad()(closure)          
+        self.first_step(zero_grad=True)
+        closure()
+        self.second_step()
+
+    def load_state_dict(self, state_dict):
+        super().load_state_dict(state_dict)
+        self.base_optimizer.param_groups = self.param_groups
