3598bf98ebc32c8101547bc7dd1837f71d6ec9bc
HoangTran223 <131913535+HoangTran223@users.noreply.github.com>
2024-09-19 17:48:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -228,13 +228,7 @@ if __name__ == "__main__":
                                                 batch_size=args.batch_size,
                                                 lr_scheduler=args.lr_scheduler,
                                                 lr_step_size=args.lr_step_size,
-<<<<<<< HEAD
-                                                rho=args.rho
-                                                )       
-
-=======
                                                 rho=args.rho)
->>>>>>> e1ad152bfa3b8b70034d2bb4e09936c03c05e94e
 
     # train the model
     # trainer.train(dataset, MOO=args.MOO_algo)
